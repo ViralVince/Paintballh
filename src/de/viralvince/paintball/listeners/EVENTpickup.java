be2a6361145a20974803202b4d7bc23ea4d34b86
ViralVince <vincent.peuker@gmail.com>
2017-07-20 18:43:31 +0200

    h

--- src/de/viralvince/paintball/listeners/EVENTpickup.java
+++ src/de/viralvince/paintball/listeners/EVENTpickup.java
@@ -1,5 +1,18 @@
 package de.viralvince.paintball.listeners;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+import de.viralvince.paintball.main.Gamestate;
+import de.viralvince.paintball.main.Main;
+
 public class EVENTpickup {
+	
+	@EventHandler
+	public void onPickup(PlayerPickupItemEvent e) {
+		if(Main.gameState == Gamestate.Ingame) {
+			e.setCancelled(true);
+		}
+	}
 
 }
